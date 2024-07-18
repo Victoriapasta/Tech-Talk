88c94f3889b44c60023f098b7b66f690613ea7cf
Victoriapasta <timel1109@naver.com>
2024-07-18 16:31:39 +0900

    feat: USER DOMAIN 관심사 컬럼 추가 및 ENUM TECH 생성

---
+++ src/main/java/toyproject/techtalk/domain/tech/Tech.java
@@ -0,0 +1,8 @@
+package toyproject.techtalk.domain.tech;
+
+import lombok.Getter;
+
+@Getter
+public enum Tech {
+    JAVA, PYTHON, JAVASCRIPT, SPRING, REACT, PYTORCH, OPENCV
+}
--- src/main/java/toyproject/techtalk/domain/user/User.java
+++ src/main/java/toyproject/techtalk/domain/user/User.java
@@ -3,6 +3,7 @@ package toyproject.techtalk.domain.user;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import toyproject.techtalk.domain.tech.Tech;
 
 @Getter
 @Entity
@@ -19,4 +20,8 @@ public class User {
     private String password;
 
     private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "USER_INTEREST")
+    private Tech interest;
 }
