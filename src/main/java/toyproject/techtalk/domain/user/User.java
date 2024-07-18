e267fdc62e83157cae99ed09bcb080fe6c85b398
Victoriapasta <timel1109@naver.com>
2024-07-18 16:27:31 +0900

    feat: User Entity create.

---
+++ src/main/java/toyproject/techtalk/domain/user/User.java
@@ -0,0 +1,22 @@
+package toyproject.techtalk.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+}
