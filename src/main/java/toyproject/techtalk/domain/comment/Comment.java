7cc754e9305815ea903e6a16e6310158a168d47c
Victoriapasta <timel1109@naver.com>
2024-07-18 16:50:41 +0900

    feat: Comment Domain 생성

---
+++ src/main/java/toyproject/techtalk/domain/comment/Comment.java
@@ -0,0 +1,32 @@
+package toyproject.techtalk.domain.comment;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import toyproject.techtalk.domain.board.Board;
+import toyproject.techtalk.domain.user.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "COMMENT_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "BOARD_ID")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    private String content;
+
+    private LocalDateTime createdTime;
+}
