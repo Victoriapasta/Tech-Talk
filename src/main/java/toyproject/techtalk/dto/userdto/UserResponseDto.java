e3120779b66deae0c43bdfa47b021104eae9d20b
Victoriapasta <timel1109@naver.com>
2024-07-18 17:39:46 +0900

    feat: userDto 생성

--- src/main/java/toyproject/techtalk/dto/userdto/UserResponseDto.java
+++ src/main/java/toyproject/techtalk/dto/userdto/UserResponseDto.java
@@ -1,4 +1,24 @@
 package toyproject.techtalk.dto.userdto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import toyproject.techtalk.domain.tech.Tech;
+import toyproject.techtalk.domain.user.User;
+
+@Getter
+@AllArgsConstructor
 public class UserResponseDto {
+
+    private Long id;
+    private String email;
+    private String password;
+    private Tech interest;
+
+    public UserResponseDto toDto(User user) {
+        return new UserResponseDto(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getInterest());
+    }
 }
