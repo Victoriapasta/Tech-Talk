e2828e5a83e09e689b6099094d817c69bcc078e0
Victoriapasta <timel1109@naver.com>
2024-07-18 19:10:24 +0900

    feat: BoardDto 생성

--- src/main/java/toyproject/techtalk/dto/boarddto/BoardRequestDto.java
+++ src/main/java/toyproject/techtalk/dto/boarddto/BoardRequestDto.java
@@ -1,4 +1,30 @@
 package toyproject.techtalk.dto.boarddto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import toyproject.techtalk.domain.board.Board;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class BoardRequestDto {
+
+    private Long id;
+    @NotBlank(message = "제목을 입력해주세요.")
+    private String title;
+    @NotBlank(message = "내용을 입력해주세요.")
+    private String content;
+    private Long userId;
+    private LocalDateTime createdTime;
+
+    public BoardRequestDto toDto(Board board) {
+        return new BoardRequestDto(
+                board.getId(),
+                board.getTitle(),
+                board.getContent(),
+                board.getUser().getId(),
+                board.getCreatedTime());
+    }
 }
--- src/main/java/toyproject/techtalk/dto/boarddto/BoardResponseDto.java
+++ src/main/java/toyproject/techtalk/dto/boarddto/BoardResponseDto.java
@@ -1,4 +1,27 @@
 package toyproject.techtalk.dto.boarddto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import toyproject.techtalk.domain.board.Board;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class BoardResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private Long userId;
+    private LocalDateTime createdTime;
+
+    public BoardResponseDto toDto(Board board) {
+        return new BoardResponseDto(
+                board.getId(),
+                board.getTitle(),
+                board.getContent(),
+                board.getUser().getId(),
+                board.getCreatedTime());
+    }
 }
