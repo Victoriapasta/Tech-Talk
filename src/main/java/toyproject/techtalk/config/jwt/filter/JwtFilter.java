aea4be7bd80aba7d66a48ed95e9444fe7b019a8b
Victoriapasta <timel1109@naver.com>
2024-07-20 22:58:14 +0900

    feat: Filter 기능 구현

--- src/main/java/toyproject/techtalk/config/jwt/filter/JwtFilter.java
+++ src/main/java/toyproject/techtalk/config/jwt/filter/JwtFilter.java
@@ -4,7 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 import toyproject.techtalk.config.jwt.provider.JwtTokenProvider;
 
@@ -12,13 +16,29 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
-    private final JwtTokenProvider jwtTokenProvider;
 
-    private static final String AUTO_HEADER = "Authorization";
+    private static final String AUTH_HEADER = "Authorization";
     private static final String BEARER = "Bearer";
     private static final String REFRESH_HEADER = "Auth";
 
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = resolveToken((HttpServletRequest) request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthenticationFromToken(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTH_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER)) {
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
