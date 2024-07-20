8cef1d0e4804908fa735c5988a33f7d529124c39
Victoriapasta <timel1109@naver.com>
2024-07-20 23:06:03 +0900

    feat: SecurityConfig 생성

---
+++ src/main/java/toyproject/techtalk/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package toyproject.techtalk.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import toyproject.techtalk.config.jwt.provider.JwtTokenProvider;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
