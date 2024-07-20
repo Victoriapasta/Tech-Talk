7d2bf2bd765dc6202b3a47f8c606fbb919ac3685
Victoriapasta <timel1109@naver.com>
2024-07-20 18:14:00 +0900

    feat: Token 계층 추가 및 Provider 생성

---
+++ src/main/java/toyproject/techtalk/config/jwt/provider/JwtTokenProvider.java
@@ -0,0 +1,22 @@
+package toyproject.techtalk.config.jwt.provider;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtTokenProvider {
+
+    private final Key key;
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    //TODO: generateToken Method
+}
---
+++ src/main/java/toyproject/techtalk/token/JwtToken.java
@@ -0,0 +1,12 @@
+package toyproject.techtalk.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class JwtToken {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
