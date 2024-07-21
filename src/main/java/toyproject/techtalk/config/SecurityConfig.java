24e76e41dc75e0e5557bedceb3e75638fe2fc802
Victoriapasta <timel1109@naver.com>
2024-07-21 21:05:29 +0900

    feat: filterChain 구현

--- src/main/java/toyproject/techtalk/config/SecurityConfig.java
+++ src/main/java/toyproject/techtalk/config/SecurityConfig.java
@@ -3,9 +3,13 @@ package toyproject.techtalk.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import toyproject.techtalk.config.jwt.provider.JwtTokenProvider;
 
 @Configuration
@@ -13,6 +17,18 @@ import toyproject.techtalk.config.jwt.provider.JwtTokenProvider;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
