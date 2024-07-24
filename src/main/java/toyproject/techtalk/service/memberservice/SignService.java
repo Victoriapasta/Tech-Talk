204a9b5415340e886eef905e46b5ee61130e93b3
Victoriapasta <timel1109@naver.com>
2024-07-24 15:15:01 +0900

    feat: signIn Service 구현

--- src/main/java/toyproject/techtalk/service/memberservice/SignService.java
+++ src/main/java/toyproject/techtalk/service/memberservice/SignService.java
@@ -1,11 +1,16 @@
 package toyproject.techtalk.service.memberservice;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.techtalk.config.jwt.provider.JwtTokenProvider;
 import toyproject.techtalk.domain.member.Member;
 import toyproject.techtalk.dto.memberdto.MemberRequestDto;
 import toyproject.techtalk.repository.MemberRepository;
+import toyproject.techtalk.token.JwtToken;
 
 @Service
 @Transactional(readOnly = true)
@@ -13,6 +18,8 @@ import toyproject.techtalk.repository.MemberRepository;
 public class SignService {
 
     private final MemberRepository memberRepository;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Transactional
     public void signUp(MemberRequestDto memberRequestDto) {
@@ -23,5 +30,17 @@ public class SignService {
                 .interest(memberRequestDto.getInterest())
                 .build();
         memberRepository.save(member);
+    }
+
+    @Transactional
+    public JwtToken signIn(String email, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+
+        Authentication authentication = authenticationManagerBuilder.getObject()
+                .authenticate(authenticationToken);
+
+        JwtToken jwtToken = jwtTokenProvider.generateToken(authentication);
+
+        return jwtToken;
     }
 }
